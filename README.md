400d2b4284c1727e7fbd30d106414c4e4681b495
alexzhang1030 <1642114555@qq.com>
2022-11-28 16:20:50 +0800

    feat: update

---
+++ README.md
@@ -0,0 +1,9 @@
+# benchmark
+
+```
+swc:
+    36 835 ops/s, ±4.20%   | fastest
+
+  babel:
+    16 814 ops/s, ±11.80%   | slowest, 54.35% slower
+```
---
+++ benchmark.js
@@ -0,0 +1,48 @@
+const code = `
+import { defineComponent as _defineComponent } from 'vue'
+import { openBlock as _openBlock, createElementBlock as _createElementBlock } from "vue"
+import { h } from 'vue'
+
+export default _defineComponent({
+    __name: 'basic',
+    setup(__props) {
+        defineRender(() => h('div'))
+        return (_ctx, _cache) => {
+            return _openBlock(), _createElementBlock("div")
+        }
+    }
+})
+`
+
+import b from 'benny'
+import * as swc from '@swc/core'
+import { transformDefineRender } from './macro.js'
+
+b.suite(
+  'Example',
+
+  b.add('swc', async () => {
+    const arr = new Array(100).fill(swc.transform(code, {
+      jsc: {
+        target: "es2016",
+        experimental: {
+          plugins: [
+            ["swc-plugin-vue-macros-define-render", {}]
+          ]
+        }
+      }
+    }))
+    Promise.all(arr)
+  }),
+
+  b.add('babel', async () => {
+    const arr = new Array(100).fill(new Promise(res => {
+      transformDefineRender(code, "index.vue")
+      res()
+    }))
+    await Promise.all(arr)
+  }),
+
+  b.cycle(),
+  b.complete(),
+)
